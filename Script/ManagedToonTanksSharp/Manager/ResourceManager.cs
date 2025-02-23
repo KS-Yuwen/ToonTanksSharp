76ad670346a013ce97ed6f877a149d4a6ed33a0e
KS-YUWEN <mugensakura3@gmail.com>
2025-02-23 12:23:25 +0900

    プロジェクトをファーストコミット

---
+++ Script/ManagedToonTanksSharp/Manager/ResourceManager.cs
@@ -0,0 +1,46 @@
+﻿using UnrealSharp.Attributes;
+using UnrealSharp.Engine;
+using UnrealSharp.EnhancedInput;
+
+namespace ManagedToonTanksSharp.Manager
+{
+    /// <summary>
+    /// リソースマネージャ
+    /// </summary>
+    [UClass]
+    public class AResourceManager : AActor
+    {
+        /// <summary>
+        /// クラスの取得
+        /// </summary>
+        /// <returns></returns>
+        public static AResourceManager Get()
+        {
+            return UGameplayStatics.GetActorOfClass<AResourceManager>();
+        }
+
+        /// <summary>
+        /// InputMappingContext
+        /// </summary>
+        [UProperty(PropertyFlags.EditAnywhere)]
+        public UInputMappingContext MappingContext { get; set; }
+
+        /// <summary>
+        /// 移動アクション
+        /// </summary>
+        [UProperty(PropertyFlags.EditAnywhere)]
+        public UInputAction MoveAction { get; set; }
+
+        /// <summary>
+        /// 発射アクション
+        /// </summary>
+        [UProperty(PropertyFlags.EditAnywhere)]
+        public UInputAction FireAction { get; set; }
+
+        /// <summary>
+        /// 視点移動アクション
+        /// </summary>
+        [UProperty(PropertyFlags.EditAnywhere)]
+        public UInputAction LookAction { get; set; }
+    }
+}
---
+++ Script/ProjectGlue/AssetIds.cs
@@ -0,0 +1,6 @@
+using UnrealSharp.CoreUObject;
+
+public static class AssetIds
+{
+	public static readonly FPrimaryAssetId Map__Game_Maps_Main = new("Map", "/Game/Maps/Main");
+}
