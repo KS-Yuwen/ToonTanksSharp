c789df6d4283e23eb02206484ccb0da27a97e319
KS-YUWEN <mugensakura3@gmail.com>
2025-02-24 19:51:59 +0900

    GameModeクラス追加

---
+++ Script/ManagedToonTanksSharp/GameMode/ToonTanksGameMode.cs
@@ -0,0 +1,13 @@
+﻿using UnrealSharp.Attributes;
+using UnrealSharp.Engine;
+
+namespace ManagedToonTanksSharp.GameMode
+{
+    /// <summary>
+    /// GameMode
+    /// </summary>
+    [UClass]
+    class AToonTanksGameMode : AGameMode
+    {
+    }
+}
